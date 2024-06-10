c3a9f5015ee53895e91a6ea8ffb5d8325d881237
kamboj7791 <kambojkunal82@gmail.com>
2024-06-11 00:17:28 +0530

    hoisting

---
+++ one.js
@@ -0,0 +1,4 @@
+console.log("value of Age is:", age); //hoisting (global execution context)
+//there are two phases i.e memory phase and code phase
+var age = 20;
+console.log("value of Age is:", age);
